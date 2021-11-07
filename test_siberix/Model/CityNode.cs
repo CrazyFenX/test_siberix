21566ba50b81a8cb25ae6daf6fddcd4053e1faf5
Кирилл <kattoshkin@gmail.com>
2021-11-08 01:04:10 +0700

    feat: add CityNode class

---
+++ test_siberix/Model/CityNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_siberix.Model
+{
+    public class CityNode
+    {
+        public City City { get; set; }
+        public ushort Distance { get; set; }
+
+        public CityNode(City _city, ushort _distance)
+        {
+            City = _city;
+            Distance = _distance;
+        }
+    }
+}
