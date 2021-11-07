86965f6ac283367b55af74d41656f0f2df01faa6
Кирилл <kattoshkin@gmail.com>
2021-11-08 01:06:55 +0700

    chore: lost of cities

--- test_siberix/Model/ListOfCities.cs
+++ test_siberix/Model/ListOfCities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace test_siberix.Model
 {
-    class ListOfCities
+    class ListOfCities : IEnumerable
     {
+        CityNode root;
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            CityNode current = root;
+            while (current != null)
+            {
+                yield return current.Distance;
+                current = current.Next;
+            }
+        }
     }
 }
