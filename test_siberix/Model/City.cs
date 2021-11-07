c33240c7d394a6b034a9e7478f113d89705a8d61
Кирилл <kattoshkin@gmail.com>
2021-11-08 01:03:44 +0700

    feat(City): add list of nearby cities and constructor

--- test_siberix/Model/City.cs
+++ test_siberix/Model/City.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace test_siberix.Model
 {
-    class City
+    public class City
     {
         public int Id { get; set; }
         public bool IsStock { get; set; }
+        public List<CityNode> NearbyCities { get; set; }
+
+        public City(int _id, bool _isStock)
+        {
+            Id = _id;
+            IsStock = _isStock;
+        }
     }
 }
